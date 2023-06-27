6f189ec9e916933405caed069480d604b7520d2e
Rabin <rabing945h@gmail.com>
2023-06-27 16:13:24 +0545

    socket io

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,30 @@
-const path = require('path');
+const path = require("path");
+const htpp = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicpath = path.join(__dirname, "/../public");
-const port = process.env.PORT|| 3000
-var app = express();
+const port = process.env.PORT || 3000;
+let app = express();
+let server = htpp.createServer(app);
+let io = socketIO(server);
+
+
 
 app.use(express.static(publicpath));
 
- app.listen(3000, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+io.on('connection', (socket) =>{
+  console.log("new user is just connected")
+
+  socket.on('disconnect', () =>{
+    console.log("User was disconnected");
   });
-  
+
+});
+
+
+
+
+server.listen(3000, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
