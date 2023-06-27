da45014f376286cb3c35434e2197a66418b733ec
Rabin <rabing945h@gmail.com>
2023-06-27 15:34:50 +0545

    server

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,12 @@ const path = require('path');
 const express = require("express");
 
 const publicpath = path.join(__dirname, "/../public");
-
+const port = process.env.PORT|| 3000
 var app = express();
 
 app.use(express.static(publicpath));
+
+ app.listen(3000, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+  
